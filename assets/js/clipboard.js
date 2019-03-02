0e95e2582c42ccec418f041cbbb207a0d666e6ee
lbeckman314 <lbeckman314@gmail.com>
2019-03-01 21:33:23 -0800

    Changed jquery location.

---
+++ assets/js/clipboard.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    let container = document.getElementsByTagName("div");
+    Array.from(container).forEach((element) => {
+
+        if (element.className == "highlight") {
+            console.log("el:", element);
+            let copyContainer = document.createElement("div");
+            copyContainer.className = "copyContainer";
+            let copySymbol = document.createElement("object");
+            //copySymbol.innerHTML = "copy!";
+            copySymbol.data = "/assets/svg/copy.svg"
+            //<object type="image/svg+xml" data="/assets/svg/heart.svg"></object>
+            copyContainer.appendChild(copySymbol);
+            //element.appendChild(copyContainer);
+            element.insertBefore(copyContainer, element.childNodes[0]);  // Insert <li> before the first child of <ul> 
+        }
+
+    });
+
+});
