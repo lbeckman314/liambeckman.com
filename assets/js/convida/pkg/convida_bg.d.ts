496b3ead5156d8376c41f19e121b3c63ffa60fcf
lbeckman314 <lbeckman314@gmail.com>
2019-04-09 16:58:00 -0700

    Updated gitignore.

---
+++ assets/js/convida/pkg/convida_bg.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+export const memory: WebAssembly.Memory;
+export function __wbindgen_global_argument_ptr(): number;
+export function __wbg_universe_free(a: number): void;
+export function universe_tick(a: number): void;
+export function universe_new(): number;
+export function universe_render(a: number, b: number): void;
+export function universe_width(a: number): number;
+export function universe_height(a: number): number;
+export function universe_cells(a: number): number;
+export function universe_set_width(a: number, b: number): void;
+export function universe_set_height(a: number, b: number): void;
+export function universe_toggle_cell(a: number, b: number, c: number): void;
+export function universe_reset(a: number): void;
+export function universe_clear(a: number): void;
+export function universe_glider(a: number, b: number, c: number): void;
+export function universe_pulsar(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number): void;
